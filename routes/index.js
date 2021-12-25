39a31d116d2c052751a84d7b5a88b8da68ae74fa
dallel-saied <dallel11@gmail.com>
2021-12-25 11:35:09 +0100

    change package-lock

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const path = require('path');
 
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// router.use(function(req, res) {
+// 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 module.exports = router;
